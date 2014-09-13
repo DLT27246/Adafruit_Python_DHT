274e5eba4868f044be7e6335797c0ba29a6cbd9f
Sancho <sanchomuzax@gmail.com>
2014-09-13 21:17:13 +0200

    Writing to log directly for RPi-Monitor
    My script is writing the data to log file directly to "/var/log/dht11.log".
    Add the following line to CRON with "crontab -e" (example):
    */1     *       *       *       * sudo /home/pi/Adafruit_Python_DHT/examples/./AdafruitDHT.py 11 4 >/dev/null 2>&1
    And rerun the CRON:
    $ sudo service cron restart

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
+# Fork: Sancho Muzax
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -45,6 +46,10 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).  
 # If this happens try again!
 if humidity is not None and temperature is not None:
-	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+        line= 'Temp = {0:0.0f} *C, Hum = {1:0.0f} %'.format(temperature, humidity)
+        print line
+        file = open("/var/log/dht11.log", "w")
+        file.write(line)
+        file.close()
 else:
-	print 'Failed to get reading. Try again!'
+        print 'Failed to get reading. Try again!'
